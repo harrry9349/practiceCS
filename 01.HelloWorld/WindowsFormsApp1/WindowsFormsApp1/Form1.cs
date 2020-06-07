c6ba1ea12ea279cf1f77b5ee5e99f327e0ff3424
harrry9349 <harrry9349@gmail.com>
2020-06-07 18:13:34 +0900

    Initial commit

---
+++ 01.HelloWorld/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            // Form1,label1のtextプロパティに「Hello,World!」と設定する
+            InitializeComponent();
+        }
+
+    }
+}
